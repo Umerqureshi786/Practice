1233365d2f26672ae0d0f8b1473c9030aa3fb371
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2024-03-14 16:00:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,197 @@
+// // Create Calculator
+
+// var firstNumber = parseInt (prompt("Enter Your First number"));
+// var secondNumber = parseInt (prompt("Enter Your second number"));
+// var operator = prompt("Enter operator");
+
+// if(operator=="+"){
+// alert(firstNumber + secondNumber)
+// }
+
+// else if(operator=="-"){
+// alert(firstNumber - secondNumber)
+
+// }
+
+// else if(operator=="*"){
+// alert(firstNumber * secondNumber)
+
+// }
+
+// else if(operator=="/"){
+// alert(firstNumber / secondNumber)
+
+// }
+
+// else {
+//     alert("You Enter Invalid Operator")
+// }
+
+
+// Create Calculator 
+// var firstNumber = parseInt(prompt("Enter first Number"));
+// var secondNumber = parseInt(prompt("Enter second Number"));
+// var operator = prompt("Enter operator")
+
+// if(operator=="+"){
+//     alert(firstNumber+secondNumber);
+// } else if(operator=="-"){
+//     alert(firstNumber-secondNumber);
+// } else if (operator=="*"){
+//     alert(firstNumber*secondNumber);
+// }else if(operator=="/"){
+//     alert(firstNumber/secondNumber);
+// } else{
+//     alert("Your enter invalid details");
+// }
+
+
+
+
+
+
+
+// Practice //
+
+
+// var abc="abc";
+// var abc;
+// abc="abc";
+// alert(abc)
+
+
+// var myWeight=50;
+// var newWeight= myWeight+20
+// alert(newWeight)
+
+// var snake_case="snakeCase";
+// var camelCase="camelCase";
+// var PascalCase="PascalCase";
+
+// alert(snake_case)
+// alert(camelCase)
+// alert(PascalCase)
+
+
+// We have 5 types of primitive data type://
+
+// var myString="Umar";
+// var myNumber=786;
+// var myboolean = true;
+// var myUndefined;
+// var myNull=  ;
+
+// // Non-primitive Data types are //
+
+// var myObject = {myName="umar" , age:26 , rollNo:175060};
+// var myArray = [];
+
+
+// var halfAge = 11
+// var halfAge2 = 11
+// var myAge = halfAge + halfAge2
+// alert(myAge)
+
+
+
+
+// var myAgeString = "23";
+// var myAgeNumber = 23;
+
+// var allAge = myAgeNumber + myAgeString;
+// alert(allAge)
+
+// var greeting = "Hello! How are You";
+// alert(greeting)
+
+
+// var versionsTill2015 = 6
+// var yearsAfter2015 = 9
+// var esVersions = versionsTill2015 + yearsAfter2015;
+// alert(esVersions);
+
+// var numberOfRows = 10;
+// var capacityOfRow = 3;
+// var totalStudents = numberOfRows * capacityOfRow;
+// alert(totalStudents);
+
+
+// var grapes = 5;
+// var numberOfPeople = 10;
+// var numberOfPieces = grapes / numberOfPeople;
+// alert(numberOfPieces);
+
+
+// var myName=prompt("Enter your Name");
+// var newName= "Mr" + myName;
+// alert(newName)
+
+// var myNum=5;
+// // var myNum= myNum + 4;
+// myNum++
+// alert(myNum)
+
+// var TotalCost= 2*2+6-10 ;
+// alert(TotalCost)
+
+
+
+// var greetingMessage = "Hello! How are you"
+// var userName = prompt("Enter Your name")
+// var abc = "123"
+// parseInt(abc)
+// var greetedUser = greetingMessage + " " + userName
+// alert(greetedUser)
+
+
+
+// var enterValue = prompt("please enter value");
+// var secondValue = prompt("please enter second value");
+
+// // for only value ==
+// // for value and data type ===
+
+// var operator = "+"
+// if (secondValue == 0) {
+//   alert("You Enter 0");
+// } else if (secondValue == 1) {
+//   alert("You Enter 1");
+// } else {
+//   alert("You Enter Other Value");
+// }   
+
+
+// var userName = prompt("Enter Your Name","Sanaullah");
+// var prefix = "Haji";
+// var user = prefix + " " + userName
+
+// alert(user)
+
+
+// var UserName3=prompt("Enter your name", "Umar Qureshi");
+// var prefix="Mr";
+// var finalName= prefix + " " + UserName3;
+// alert(finalName)
+
+
+// var isInstructor1Present = prompt("teacher1 a gye", "Yes");
+// var isInstructor2Present = prompt("teacher2 a gye", "Yes");
+// var isStudentPresent = prompt("bache a gye", "Yes");
+
+// if (isInstructor2Present === "yes" && isStudentPresent == "yes") {
+//     alert("Start Class")
+// } else {
+//     alert("Class me betho")
+// }
+
+// var htmlQuiz = prompt ("Enter if you passed Html Quiz", "yes");
+// var cssQuiz =prompt ("Enter if you passed Css Quiz", "yes");
+
+// if(htmlQuiz==="yes" || cssQuiz==="yes"){
+//     alert("You have passed");
+// }else {
+//     alert("You failed");
+// }
+
+
+
