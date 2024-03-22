7614aeb6784d32730eaad2e5155665ec2d5336bd
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2024-03-22 15:08:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -174,24 +174,24 @@
 // alert(finalName)
 
 
-// var isInstructor1Present = prompt("teacher1 a gye", "Yes");
-// var isInstructor2Present = prompt("teacher2 a gye", "Yes");
-// var isStudentPresent = prompt("bache a gye", "Yes");
+var isInstructor1Present = prompt("teacher1 a gye", "Yes");
+var isInstructor2Present = prompt("teacher2 a gye", "Yes");
+var isStudentPresent = prompt("bache a gye", "Yes");
 
-// if (isInstructor2Present === "yes" && isStudentPresent == "yes") {
-//     alert("Start Class")
-// } else {
-//     alert("Class me betho")
-// }
+if (isInstructor2Present === "yes" && isStudentPresent == "yes") {
+    alert("Start Class")
+} else {
+    alert("Class me betho")
+}
 
-// var htmlQuiz = prompt ("Enter if you passed Html Quiz", "yes");
-// var cssQuiz =prompt ("Enter if you passed Css Quiz", "yes");
+var htmlQuiz = prompt ("Enter if you passed Html Quiz", "yes");
+var cssQuiz =prompt ("Enter if you passed Css Quiz", "yes");
 
-// if(htmlQuiz==="yes" || cssQuiz==="yes"){
-//     alert("You have passed");
-// }else {
-//     alert("You failed");
-// }
+if(htmlQuiz==="yes" || cssQuiz==="yes"){
+    alert("You have passed");
+}else {
+    alert("You failed");
+}
 
   / var student1 = "Ali";
 // var student2 = "Umar";
@@ -199,65 +199,244 @@
 
 // console.log("var:" , student1,student2,student3);
 
-// var students = ["Ali", "Umar", "Usman"];
-// console.log("Array:", students);
-// document.write(students[2]);
-// alert(students)
+var students = ["Ali", "Umar", "Usman"];
+console.log("Array:", students);
+document.write(students[2]);
+alert(students)
 
-// var students=["Rehan", "Farhan"];
-// var student1= prompt("Enter student name");
+var students=["Rehan", "Farhan"];
+var student1= prompt("Enter student name");
 
-// students.push(student1)
-// console.log(students);
-
-
-
-// var students=["Rehan", "Farhan","Ali"];
-// students.pop()
-// console.log(students);
+students.push(student1)
+console.log(students);
 
 
 
-// var students = ["Student1","Student2","Student3"]
-// students.pop()
-
-// console.log(students);
-
-// var myUsers = [];
-// var userName = prompt("User name");
-// myUsers.push("umar");
-// myUsers.push("haider");
-// myUsers.push("salman");
-// myUsers.push(userName)
-// myUsers.unshift("aliiiiii")
-// myUsers.shift()
-
-// console.log(myUsers);
-
-
-// myUsers.pop();
-// console.log(myUsers);
-
-
-// var students = ["ali", "umar", "fazal","new"];
-// students.splice(1,2)
-// // students.splice(1,1,"uzair","haidar")
-
-// console.log(students);
+var students=["Rehan", "Farhan","Ali"];
+students.pop()
+console.log(students);
 
 
 
-// var students=["a", "b", "c", "d", "e"];
-// students.push("Z","X");
-// students.pop();
-// students.unshift("G");
-// students.shift();
-// // students.splice(1,2);
-// // students.slice(0,1);
-// console.log(students);
+var students = ["Student1","Student2","Student3"]
+students.pop()
+
+console.log(students);
+
+var myUsers = [];
+var userName = prompt("User name");
+myUsers.push("umar");
+myUsers.push("haider");
+myUsers.push("salman");
+myUsers.push(userName)
+myUsers.unshift("aliiiiii")
+myUsers.shift()
+
+console.log(myUsers);
+
+
+myUsers.pop();
+console.log(myUsers);
+
+
+var students = ["ali", "umar", "fazal","new"];
+students.splice(1,2)
+// students.splice(1,1,"uzair","haidar")
+
+console.log(students);
+
+
+
+var students=["a", "b", "c", "d", "e"];
+students.push("Z","X");
+students.pop();
+students.unshift("G");
+students.shift();
+// students.splice(1,2);
+// students.slice(0,1);
+console.log(students);
 
 
 var students = ["a", "b", "c", "d", "e", "f"];
 var newArray = students.slice(0,4);
 console.log(newArray);
 
+
+
+var students = ["a", "b", "c", "d", "e", "f"];
+var newArray = students.slice(0,2);
+console.log(newArray);
+
+
+var a=5;
+var a=6;
+console.log(a);
+
+let a=5;
+ a=8;
+console.log(a)
+
+const a=5;
+console.log(a);
+
+
+
+
+var UserName=["Umar", "Ahsan", "Iqra", "Ujalay", "Ali", "Usman"];
+UserName.push("Juma","Salman")
+UserName.pop()
+UserName.unshift("Abdullah")
+UserName.shift()
+UserName.splice(1,2)
+UserName.slice(0,3)
+
+console.log(UserName);
+
+
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+
+
+for (var i = 5; i > 0; i--) {
+  console.log(i);
+}
+
+
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray.length); 
+
+let myString = "Hello world!";
+console.log(myString.length); 
+
+
+
+var newArray = ["a", "b", "c", "d", "e", "f"];
+for (var i = newArray.length - 1; i >= 0; i--) {
+  console.log(newArray[i]);
+}
+
+
+var students = ["zeeshan", "IT", "haider", "World YT"];
+for (var i = 0; i < students.length; i++) {
+  console.log(students[i]);
+  if (students[i] === "IT") {
+    break;
+  }
+  console.log("console2", students[i]);
+}
+
+
+var maritalStatus = prompt("Are You Married");
+var casesMaritalStatus = maritalStatus.toLowerCase();
+
+console.log(maritalStatus, casesMaritalStatus, "case");
+
+console.log(maritalStatus.toUpperCase(), "Upper");
+if (maritalStatus.toUpperCase() === "YES") {
+  console.log("Mubarak");
+} else {
+  console.log("Earn some money");
+}
+
+
+var myName = "ubaid";
+var firstLetter = myName.slice(0,2);
+// var remainingLetter = myName.slice(1);
+
+var newName = firstLetter.toUpperCase();
+console.log(myName, newName);
+
+var newArray = ["a", "b", "c", "d", "e", "f"];
+for (var i = newArray.length - 1; i >= 0; i--) {
+  console.log(newArray[i]);
+}
+
+
+var students = ["zeeshan", "IT", "haider", "World YT"];
+for (var i = 0; i < students.length; i++) {
+  console.log(students[i]);
+  if (students[i] === "IT") {
+    continue;
+  }
+  console.log("console2", students[i]);
+}
+
+var maritalStatus = prompt("Enter are you married");
+ maritalStatus.toUpperCase()
+    // console.log(maritalStatus, newMaritalStatus);
+    if(maritalStatus.toUpperCase() ==="YES"){
+console.log("Mubarak ho");
+    } 
+    else {
+        console.log("Earn some money");
+    }
+
+
+    if (maritalStatus.toUpperCase() === "YES") {
+  console.log("Mubarak");
+} else {
+  console.log("Earn some money");
+}
+
+
+var myName="Umar";
+var firstLetter= myName.slice(0,2);
+var remainingLetter=myName.slice(1);
+var newName=firstLetter.toUpperCase() + remainingLetter.toLowerCase();
+
+console.log(newName);
+
+var studentName = "Ahsan is good student";
+for (var i = 0; i < studentName.length; i++) {
+  console.log(studentName[i]);
+}
+
+
+var studentName = "Rehaaan khan";
+for(i=0; i < studentName.length; i++){
+console.log(studentName[i]);
+}
+
+
+var iIndex = -1;
+var variable1 = "Thihsh";
+console.log(variable1.lastIndexOf("h"));
+for (var i = 0; i < variable1.length; i++) {
+  if (variable1.charAt(i) === "h") {
+    iIndex = i;
+    break;
+  }
+}
+console.log(iIndex);
+console.log(variable1.charAt(0), variable1[0]);
+
+
+var stringValue = "World War Two was biggest war ever";
+var twoIndex = stringValue.indexOf("Two"); //10
+var string1 = stringValue.slice(0, twoIndex);
+var string2 = stringValue.slice(twoIndex + 3);
+
+var newString = string1 + "2" + string2;
+console.log(newString);
+
+// alert("Alhamdulillah");
+ var originalNum = "23";
+ var newNum = originalNum + 7;
+console.log(newNum);
+
+var spec = prompt("Your species?", "human");
+
+
+var question = "Your species?";
+var defaultAnswer = "huma";
+var spec = prompt(question, defaultAnswer);
+
+var myName=prompt("Umar", "yes");
+if (myName==="yes"){
+    console.log(true);
+} else {
+    console.log("Not valid")
+}
